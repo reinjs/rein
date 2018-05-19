46407e3184f06a2e6032b07254ee9a5d1202a8a1
shenyunjie <shenyunjie@u51.com>
2018-05-19 21:18:27 +0800

    [up] update rein-loader

--- agent.js
+++ agent.js
@@ -17,6 +17,15 @@ module.exports = class AgentRuntime extends Agent {
     return this;
   }
   
+  async _exec(action, data) {
+    const re = /^(([^:]+):)?(.+)$/.exec(action);
+    const name = re[2], event = re[3];
+    if (!event) return;
+    if (name && this.router && this.router[name] && this.router[name][event]) return await this.router[name][event](data);
+    if (this.router && this.router.project && this.router.project[event]) return await this.router.project[event](data);
+    return await this.emit(event, data);
+  }
+  
   /**
    * receive message from upstream
    * @param msg
@@ -26,7 +35,7 @@ module.exports = class AgentRuntime extends Agent {
     if (msg.action === 'cluster:ready') return await this._app.invoke('ready');
     if (msg.body && msg.body.__ipc_callback__) {
       const id = msg.body.__ipc_callback__;
-      const res = await this.emit(msg.action, msg.body.data).catch(e => Promise.resolve(e));
+      const res = await this._exec(msg.action, msg.body.data).catch(e => Promise.resolve(e));
       if (is.error(res)) {
         this.send(msg.from, id, { error: res.message });
         return;
--- test/app/book.js
+++ test/app/book.js
@@ -1,5 +1,15 @@
-module.exports = app => {
-  app.receive('test1', async data => {
+// module.exports = app => {
+//   app.receive('test1', async data => {
+//     return data + 'shenyunjie';
+//   });
+// };
+
+module.exports = class Message {
+  constructor(app) {
+    this.app = app;
+  }
+  
+  async test1(data) {
     return data + 'shenyunjie';
-  });
+  }
 };
